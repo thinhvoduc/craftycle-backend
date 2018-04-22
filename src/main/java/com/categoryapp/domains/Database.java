435d7cebca9dae45f8b7a5778a24b3f218825b60
Thinh Vo <ducthinhducthinh2410@gmail.com>
2018-04-23 02:23:50 +0300

    Made changes to categories set

--- src/main/java/com/categoryapp/domains/Database.java
+++ src/main/java/com/categoryapp/domains/Database.java
@@ -20,13 +20,14 @@ public class Database {
         
         // Pre-defined categories
         String baseUrl = "http://localhost:8080/CategoryApp/webresources/images/";
-        Category category0 = new Category(0, "Animal", baseUrl + "animal.png");
-        Category category1 = new Category(1, "Vehicle", baseUrl + "vehicle.png");
-        Category category2 = new Category(2, "Girl", baseUrl + "girl.png");
-        Category category3 = new Category(3, "Military", baseUrl + "military.png");
-        Category category4 = new Category(4, "Anime", baseUrl + "anime.png");
-        Category category5 = new Category(5, "Love", baseUrl + "love.png");
-        Category category6 = new Category(6, "Celebrity", baseUrl + "celebrity.png");
+        Category category0 = new Category(0, "Old device", baseUrl + "animal.png");
+        Category category1 = new Category(1, "Wood", baseUrl + "vehicle.png");
+        Category category2 = new Category(2, "Paper", baseUrl + "girl.png");
+        Category category3 = new Category(3, "Metal", baseUrl + "military.png");
+        Category category4 = new Category(4, "Fabric", baseUrl + "anime.png");
+        Category category5 = new Category(5, "Decoration", baseUrl + "love.png");
+        Category category6 = new Category(6, "Everyday Waste", baseUrl + "celebrity.png");
+        Category category7 = new Category(6, "Problematic Waste", baseUrl + "celebrity.png");
         
         categories.add(category0);
         categories.add(category1);
@@ -35,6 +36,7 @@ public class Database {
         categories.add(category4);
         categories.add(category5);
         categories.add(category6);
+        categories.add(category7);
     }
     
     // public static Database 

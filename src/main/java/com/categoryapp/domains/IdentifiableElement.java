8b84e347dca72f82a11c64b8e220c73ee6fcd16e
Thinh Vo <ducthinh2410@gmail.com>
2018-04-26 22:16:45 +0300

    Created IdentifiableElement interface

---
+++ src/main/java/com/categoryapp/domains/IdentifiableElement.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.categoryapp.domains;
+
+/**
+ *
+ * @author thinh
+ */
+public interface IdentifiableElement {
+    public Integer getId();
+    public void setId(Integer id);
+}
